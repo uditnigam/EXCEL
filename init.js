c33a84cdcff10c423ea21ddd6ba47ad5b8ebce0f
Udit Nigam <rajnigam.rock443@gmail.com>
2023-09-10 18:56:41 +0530

    Add grid to the UI

--- init.js
+++ init.js
@@ -1,22 +1,32 @@
 const topRightRow = document.querySelector(".top-right-row");
 const leftColumn = document.querySelector(".left-column");
 const grid = document.querySelector(".grid");
+
 let str = '';
-let number = '';
 
-
+//Loop for creating Top Right Row of Grid
 for (let i = 0; i < 26; i++) {
     str = str + `<div class="col">${String.fromCharCode(65 + i)}</div>`;  // To convert numbers into Alphabets using ASCII Code
 };
 topRightRow.innerHTML = str;
- 
+
+//Loop for creating Left Column of Grid
+str = '';
 for (let i = 0; i < 100; i++) {
-    number = number + `<div class="row">${1 + i}</div>`
+    str += `<div class="row">${1 + i}</div>`
 };
-leftColumn.innerHTML = number;
+leftColumn.innerHTML = str;
 
-for (let i = 0; i < 26; i++){
-    for (let j = 100; j < i; j++){
-
+//Nested Loop for creating Rows and Columns in Grid
+str = '';
+for (let i = 0; i < 100; i++) {
+    str += `<div class="row">`
+    for (let j = 0; j < 26; j++) {
+        str += `<div class="col"></div>`
     }
-}
+    str +=`</div>`
+};
+grid.innerHTML = str;
+
+
+
